5b00e8d74000b16aadf1d755f5ed73be2c1717cc
austinwasingerprog <austin_s_wasinger@progressive.com>
2020-09-17 18:27:27 +0000

    removed angular material from dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
-import { LeagueComponent } from './pages/league/league.component';
-import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppAngularMaterialModule } from './modules/app-angular-material.module';
 import { AppRouterModule } from './modules/app-router.module';
-import { CreateLeagueComponent } from './pages/create-league/create-league.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginLeagueComponent } from './pages/login-league/login-league.component';
-import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeagueComponent,
-    NavBarComponent,
-    CreateLeagueComponent,
-    HomeComponent,
-    LoginLeagueComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FlexLayoutModule,
-    AppAngularMaterialModule,
     AppRouterModule
   ],
   providers: [
